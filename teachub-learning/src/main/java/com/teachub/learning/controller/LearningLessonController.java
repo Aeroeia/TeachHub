212f2ca01a006c09664aeab44818406d65e53b12
A <1269938269@qq.com>
2025-01-16 17:28:36 +0800

    学习课表-表结构设计

---
+++ teachub-learning/src/main/java/com/teachub/learning/controller/LearningLessonController.java
@@ -0,0 +1,20 @@
+package com.teachub.learning.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 学生课程表 前端控制器
+ * </p>
+ *
+ * @author aer
+ * @since 2025-06-26
+ */
+@RestController
+@RequestMapping("/learning-lesson")
+public class LearningLessonController {
+
+}
