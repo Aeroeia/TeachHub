ef3cdb90bb9283ee828579c3ba12c2c909524185
Aeroeia <1269938269@qq.com>
2025-02-23 17:41:00 +0800

    用户端分页查询回答or评论 && 提交问题回答

--- teachub-learning/src/main/java/com/teachub/learning/controller/InteractionReplyController.java
+++ teachub-learning/src/main/java/com/teachub/learning/controller/InteractionReplyController.java
@@ -1,9 +1,16 @@
 package com.teachub.learning.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.teachub.common.domain.dto.PageDTO;
+import com.teachub.learning.domain.dto.ReplyDTO;
+import com.teachub.learning.domain.dto.ReplyPageQuery;
+import com.teachub.learning.domain.vo.ReplyVO;
+import com.teachub.learning.service.IInteractionReplyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -11,10 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
  * </p>
  *
  * @author Aeroeia
- * @since 2025-07-20
+ * @since 2025-07-19
  */
+@Slf4j
 @RestController
-@RequestMapping("/interaction-reply")
+@RequestMapping("/replies")
+@RequiredArgsConstructor
+@Api(tags = "互动问题回答、评论接口")
 public class InteractionReplyController {
+    private final IInteractionReplyService replyService;
+    @ApiOperation("提交问题回答")
+    @PostMapping
+    public void postReply(@RequestBody ReplyDTO replyDTO){
+        log.info("问题提交表单:{}",replyDTO);
+        replyService.postReply(replyDTO);
+    }
+    @ApiOperation("分页查询问题回答")
+    @GetMapping("/page")
+    public PageDTO<ReplyVO> pageQuery(ReplyPageQuery replyPageQuery){
+        log.info("分页查询参数:{}",replyPageQuery);
+        return replyService.pageQuery(replyPageQuery);
+    }
 
 }
--- teachub-learning/src/main/java/com/teachub/learning/service/IInteractionReplyService.java
+++ teachub-learning/src/main/java/com/teachub/learning/service/IInteractionReplyService.java
@@ -1,7 +1,11 @@
 package com.teachub.learning.service;
 
+import com.teachub.common.domain.dto.PageDTO;
+import com.teachub.learning.domain.dto.ReplyDTO;
+import com.teachub.learning.domain.dto.ReplyPageQuery;
 import com.teachub.learning.domain.po.InteractionReply;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.teachub.learning.domain.vo.ReplyVO;
 
 /**
  * <p>
@@ -13,4 +17,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IInteractionReplyService extends IService<InteractionReply> {
 
+    void postReply(ReplyDTO replyDTO);
+
+    PageDTO<ReplyVO> pageQuery(ReplyPageQuery replyPageQuery);
 }
