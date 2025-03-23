334216686147da0c8e1addce370039232e3afec3
Aeroeia <1269938269@qq.com>
2025-03-23 11:45:59 +0800

    查看发放的优惠券

---
+++ teachub-promotion/src/main/java/com/teachub/promotion/service/IUserCouponService.java
@@ -0,0 +1,16 @@
+package com.teachub.promotion.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.teachub.promotion.domain.po.UserCoupon;
+
+/**
+ * <p>
+ * 用户领取优惠券的记录，是真正使用的优惠券信息 服务类
+ * </p>
+ *
+ * @author Aeroeia
+ * @since 2025-08-01
+ */
+public interface IUserCouponService extends IService<UserCoupon> {
+
+}
