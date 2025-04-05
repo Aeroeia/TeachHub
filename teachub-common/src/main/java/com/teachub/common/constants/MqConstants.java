d0aa74a494dda9b2de313061cf5b62f4b0eef2df
Aeroeia <1269938269@qq.com>
2025-04-05 23:45:40 +0800

    引入RocketMQ

--- teachub-common/src/main/java/com/teachub/common/constants/MqConstants.java
+++ teachub-common/src/main/java/com/teachub/common/constants/MqConstants.java
@@ -1,84 +1,79 @@
 package com.teachub.common.constants;
 
 public interface MqConstants {
-    interface Exchange{
+    interface Topic {
         /*课程有关的交换机*/
-        String COURSE_EXCHANGE = "course.topic";
+        String COURSE_TOPIC = "course_topic";
 
         /*订单有关的交换机*/
-        String ORDER_EXCHANGE = "order.topic";
+        String ORDER_TOPIC = "order_topic";
 
         /*学习有关的交换机*/
-        String LEARNING_EXCHANGE = "learning.topic";
+        String LEARNING_TOPIC = "learning_topic";
 
         /*信息中心短信相关的交换机*/
-        String SMS_EXCHANGE = "sms.direct";
+        String SMS_TOPIC = "sms_topic";
 
         /*异常信息的交换机*/
-        String ERROR_EXCHANGE = "error.topic";
+        String ERROR_TOPIC = "error_topic";
 
         /*支付有关的交换机*/
-        String PAY_EXCHANGE = "pay.topic";
+        String PAY_TOPIC = "pay_topic";
         /*交易服务延迟任务交换机*/
-        String TRADE_DELAY_EXCHANGE = "trade.delay.topic";
+        String TRADE_DELAY_TOPIC = "trade_delay_topic";
 
         /*点赞记录有关的交换机*/
-        String LIKE_RECORD_EXCHANGE = "like.record.topic";
+        String LIKE_RECORD_TOPIC = "like_record_topic";
 
         //促销服务交换机
-        String PROMOTION_EXCHANGE = "promotion.topic";
+        String PROMOTION_TOPIC = "promotion_topic";
     }
-    interface Queue {
-        String ERROR_QUEUE_TEMPLATE = "error.{}.queue";
-        //课表相关队列
-        String LEARNING_LESSON_PAY_QUEUE = "learning_lesson_pay.queue";
-        String LEARNING_LESSON_REFUND_QUEUE = "learning_lesson_refund.queue";
-    }
-    interface Key{
+    
+    interface Tag {
         /*课程有关的 RoutingKey*/
-        String COURSE_NEW_KEY = "course.new";
-        String COURSE_UP_KEY = "course.up";
-        String COURSE_DOWN_KEY = "course.down";
-        String COURSE_EXPIRE_KEY = "course.expire";
-        String COURSE_DELETE_KEY = "course.delete";
+        String COURSE_NEW = "course_new";
+        String COURSE_UP = "course_up";
+        String COURSE_DOWN = "course_down";
+        String COURSE_EXPIRE = "course_expire";
+        String COURSE_DELETE = "course_delete";
 
         /*订单有关的RoutingKey*/
-        String ORDER_PAY_KEY = "order.pay";
-        String ORDER_REFUND_KEY = "order.refund";
+        String ORDER_PAY = "order_pay";
+        String ORDER_REFUND = "order_refund";
 
         /*积分相关RoutingKey*/
         /* 写回答 */
-        String WRITE_REPLY = "reply.new";
+        String WRITE_REPLY = "reply_new";
         /* 签到 */
-        String SIGN_IN = "sign.in";
+        String SIGN_IN = "sign_in";
         /* 学习视频 */
-        String LEARN_SECTION = "section.learned";
+        String LEARN_SECTION = "section_learned";
         /* 写笔记 */
-        String WRITE_NOTE = "note.new";
+        String WRITE_NOTE = "note_new";
         /* 笔记被采集 */
-        String NOTE_GATHERED = "note.gathered";
+        String NOTE_GATHERED = "note_gathered";
 
         /*点赞的RoutingKey*/
-        String LIKED_TIMES_KEY_TEMPLATE = "{}.times.changed";
+        String LIKED_TIMES_KEY_TEMPLATE = "{}_times_changed";
         /*问答*/
-        String QA_LIKED_TIMES_KEY = "QA.times.changed";
+        String QA_LIKED_TIMES_KEY = "QA_times_changed";
         /*笔记*/
-        String NOTE_LIKED_TIMES_KEY = "NOTE.times.changed";
+        String NOTE_LIKED_TIMES_KEY = "NOTE_times_changed";
 
         /*短信系统发送短信*/
-        String SMS_MESSAGE = "sms.message";
+        String SMS_MESSAGE = "sms_message";
 
         /*异常RoutingKey的前缀*/
-        String ERROR_KEY_PREFIX = "error.";
-        String DEFAULT_ERROR_KEY = "error.#";
+        String ERROR_KEY_PREFIX = "error_";
+        String DEFAULT_ERROR_TAG = "error_all";
 
         /*支付有关的key*/
-        String PAY_SUCCESS = "pay.success";
-        String REFUND_CHANGE = "refund.status.change";
+        String PAY_SUCCESS = "pay_success";
+        String REFUND_CHANGE = "refund_status_change";
 
-        String ORDER_DELAY_KEY = "delay.order.query";
+        String ORDER_DELAY = "delay_order_query";
 
         //领取优惠券的key
-        String COUPON_RECEIVE = "coupon.receive";
+        String COUPON_RECEIVE = "coupon_receive";
     }
 }
