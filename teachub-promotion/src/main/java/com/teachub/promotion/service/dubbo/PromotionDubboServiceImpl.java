2bd2c6ebb99c1505d12ec9a998ac03cc676b0be1
Aeroeia <1269938269@qq.com>
2025-04-11 00:04:54 +0800

    dubbo重构

--- teachub-promotion/src/main/java/com/teachub/promotion/service/dubbo/PromotionDubboServiceImpl.java
+++ teachub-promotion/src/main/java/com/teachub/promotion/service/dubbo/PromotionDubboServiceImpl.java
@@ -3,6 +3,7 @@ package com.teachub.promotion.service.dubbo;
 import com.teachub.api.dto.promotion.CouponDiscountDTO;
 import com.teachub.api.dto.promotion.OrderCourseDTO;
 import com.teachub.api.promotion.PromotionClient;
+import com.teachub.common.utils.BeanUtils;
 import com.teachub.promotion.service.IUserCouponService;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,19 @@ public class PromotionDubboServiceImpl implements PromotionClient {
 
     @Override
     public List<CouponDiscountDTO> findDiscountSolution(List<OrderCourseDTO> courseDTOS) {
-        return userCouponService.findDiscountSolution(courseDTOS);
+        // 1. Convert API DTOs to Domain DTOs
+        List<com.teachub.promotion.domain.dto.OrderCourseDTO> domainDTOS = BeanUtils.copyList(courseDTOS, com.teachub.promotion.domain.dto.OrderCourseDTO.class);
+        
+        // 2. Call service
+        List<com.teachub.promotion.domain.dto.CouponDiscountDTO> domainResult = null;
+        try {
+            domainResult = userCouponService.findDiscountSolution(domainDTOS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return List.of();
+        }
+
+        // 3. Convert Domain DTOs back to API DTOs
+        return BeanUtils.copyList(domainResult, CouponDiscountDTO.class);
     }
 }
