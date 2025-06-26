54debe1cb7d300cd94d2f9edf174a92df0367088
A <1269938269@qq.com>
2025-06-26 14:20:29 +0800

    学习课表-表结构设计

---
+++ teachub-learning/src/main/java/com/teachub/learning/service/ILearningLessonService.java
@@ -0,0 +1,16 @@
+package com.teachub.learning.service;
+
+import com.teachub.learning.domain.po.LearningLesson;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 学生课程表 服务类
+ * </p>
+ *
+ * @author aer
+ * @since 2025-06-26
+ */
+public interface ILearningLessonService extends IService<LearningLesson> {
+
+}
---
+++ teachub-learning/src/main/java/com/teachub/learning/service/impl/LearningLessonServiceImpl.java
@@ -0,0 +1,20 @@
+package com.teachub.learning.service.impl;
+
+import com.teachub.learning.domain.po.LearningLesson;
+import com.teachub.learning.mapper.LearningLessonMapper;
+import com.teachub.learning.service.ILearningLessonService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 学生课程表 服务实现类
+ * </p>
+ *
+ * @author aer
+ * @since 2025-06-26
+ */
+@Service
+public class LearningLessonServiceImpl extends ServiceImpl<LearningLessonMapper, LearningLesson> implements ILearningLessonService {
+
+}
