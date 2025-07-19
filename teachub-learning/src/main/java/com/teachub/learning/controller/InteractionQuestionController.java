cdb8be5c43227a0f514c8b8047079ad31cce2ecc
Aeroeia <1269938269@qq.com>
2025-07-20 04:59:58 +0800

    用户端分页查询问题

--- teachub-learning/src/main/java/com/teachub/learning/controller/InteractionQuestionController.java
+++ teachub-learning/src/main/java/com/teachub/learning/controller/InteractionQuestionController.java
@@ -1,7 +1,10 @@
 package com.teachub.learning.controller;
 
 
+import com.teachub.common.domain.dto.PageDTO;
 import com.teachub.learning.domain.dto.QuestionFormDTO;
+import com.teachub.learning.domain.dto.QuestionPageQuery;
+import com.teachub.learning.domain.vo.QuestionVO;
 import com.teachub.learning.service.IInteractionQuestionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -36,5 +39,11 @@ public class InteractionQuestionController {
     public void updateQuestion(@RequestBody QuestionFormDTO questionFormDTO,@PathVariable Long id){
         log.info("问题:{}",questionFormDTO);
         interactionQuestionService.updateQuestion(questionFormDTO,id);
+    }
+    @ApiOperation("分页查询问题")
+    @GetMapping("/page")
+    public PageDTO<QuestionVO> queryQuestions(@Validated QuestionPageQuery pageQuery){
+        log.info("分页查询参数:{}",pageQuery);
+        return interactionQuestionService.queryQuestions(pageQuery);
     }
 }
--- teachub-learning/src/main/java/com/teachub/learning/service/impl/InteractionQuestionServiceImpl.java
+++ teachub-learning/src/main/java/com/teachub/learning/service/impl/InteractionQuestionServiceImpl.java
@@ -1,16 +1,31 @@
 package com.teachub.learning.service.impl;
 
 import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.teachub.api.client.user.UserClient;
+import com.teachub.api.dto.user.UserDTO;
+import com.teachub.common.domain.dto.PageDTO;
 import com.teachub.common.exceptions.BadRequestException;
 import com.teachub.common.exceptions.BizIllegalException;
 import com.teachub.common.utils.BeanUtils;
+import com.teachub.common.utils.CollUtils;
 import com.teachub.common.utils.UserContext;
 import com.teachub.learning.domain.dto.QuestionFormDTO;
+import com.teachub.learning.domain.dto.QuestionPageQuery;
 import com.teachub.learning.domain.po.InteractionQuestion;
+import com.teachub.learning.domain.po.InteractionReply;
+import com.teachub.learning.domain.vo.QuestionVO;
 import com.teachub.learning.mapper.InteractionQuestionMapper;
 import com.teachub.learning.service.IInteractionQuestionService;
+import com.teachub.learning.service.IInteractionReplyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -21,12 +36,15 @@ import org.springframework.stereotype.Service;
  * @since 2025-07-19
  */
 @Service
+@RequiredArgsConstructor
 public class InteractionQuestionServiceImpl extends ServiceImpl<InteractionQuestionMapper, InteractionQuestion> implements IInteractionQuestionService {
+    private final UserClient userClient;
+    private final IInteractionReplyService replyService;
 
     @Override
     public void addQuestion(QuestionFormDTO questionFormDTO) {
         Long userId = UserContext.getUser();
-        if(userId==null){
+        if (userId == null) {
             throw new BadRequestException("用户未登录");
         }
         InteractionQuestion interactionQuestion = BeanUtils.copyBean(questionFormDTO, InteractionQuestion.class);
@@ -35,16 +53,16 @@ public class InteractionQuestionServiceImpl extends ServiceImpl<InteractionQuest
     }
 
     @Override
-    public void updateQuestion(QuestionFormDTO questionFormDTO,Long id) {
+    public void updateQuestion(QuestionFormDTO questionFormDTO, Long id) {
         Long userId = UserContext.getUser();
-        if(userId==null){
+        if (userId == null) {
             throw new BadRequestException("用户未登录");
         }
         InteractionQuestion interactionQuestion = this.lambdaQuery().eq(InteractionQuestion::getId, id).one();
-        if(interactionQuestion==null){
+        if (interactionQuestion == null) {
             throw new BadRequestException("问题不存在");
         }
-        if(!interactionQuestion.getUserId().equals(userId)){
+        if (!interactionQuestion.getUserId().equals(userId)) {
             throw new BizIllegalException("不能修改他人问题");
         }
         String title = questionFormDTO.getTitle();
@@ -53,7 +71,58 @@ public class InteractionQuestionServiceImpl extends ServiceImpl<InteractionQuest
         this.lambdaUpdate().eq(InteractionQuestion::getId, id)
                 .set(StrUtil.isNotBlank(title), InteractionQuestion::getTitle, title)
                 .set(StrUtil.isNotBlank(description), InteractionQuestion::getDescription, description)
-                .set(anonymity!=null, InteractionQuestion::getAnonymity, anonymity)
+                .set(anonymity != null, InteractionQuestion::getAnonymity, anonymity)
                 .update();
+    }
+
+    @Override
+    public PageDTO<QuestionVO> queryQuestions(QuestionPageQuery pageQuery) {
+        Long userId = UserContext.getUser();
+        if (userId == null) {
+            throw new BadRequestException("用户未登录");
+        }
+        //构造分页参数
+        Page<InteractionQuestion> page = pageQuery.toMpPageDefaultSortByCreateTimeDesc();
+        Page<InteractionQuestion> p = this.lambdaQuery().eq(pageQuery.getOnlyMine() != null && pageQuery.getOnlyMine(), InteractionQuestion::getUserId, userId)
+                .eq(InteractionQuestion::getCourseId, pageQuery.getCourseId())
+                .eq(pageQuery.getSectionId() != null, InteractionQuestion::getSectionId, pageQuery.getSectionId())
+                .eq(InteractionQuestion::getHidden, false)
+                //排出description
+                .select(InteractionQuestion.class, new Predicate<TableFieldInfo>() {
+                    @Override
+                    public boolean test(TableFieldInfo tableFieldInfo) {
+                        return !tableFieldInfo.getProperty().equals("description");
+                    }
+                })
+                .page(page);
+        List<InteractionQuestion> records = p.getRecords();
+        if (CollUtils.isEmpty(records)) {
+            return PageDTO.empty(p);
+        }
+        //根据最新回答用户id进行分类
+        List<Long> answerIds = records.stream().map(InteractionQuestion::getLatestAnswerId).filter(Objects::nonNull).collect(Collectors.toList());
+        List<InteractionReply> interactionReplies = replyService.listByIds(answerIds);
+        Set<Long> collect = interactionReplies.stream().map(InteractionReply::getUserId).collect(Collectors.toSet());
+        List<Long> userIds = records.stream().map(InteractionQuestion::getUserId).collect(Collectors.toList());
+        collect.addAll(userIds);
+        List<UserDTO> users = userClient.queryUserByIds(collect);
+        Map<Long, UserDTO> userMap = users.stream().collect(Collectors.toMap(UserDTO::getId, u -> u));
+        Map<Long, InteractionReply> interactionReplyMap = interactionReplies.stream().collect(Collectors.toMap(InteractionReply::getId, r -> r));
+        List<QuestionVO> result = new ArrayList<>();
+        //po->vo
+        for (InteractionQuestion question : records) {
+            QuestionVO questionVO = BeanUtils.toBean(question, QuestionVO.class);
+            if(!questionVO.getAnonymity()){
+                questionVO.setUserName(userMap.get(question.getUserId()).getName());
+                questionVO.setUserIcon(userMap.get(question.getUserId()).getIcon());
+            }
+            if (question.getLatestAnswerId() != null) {
+                questionVO.setLatestReplyContent(interactionReplyMap.get(question.getLatestAnswerId()).getContent());
+                questionVO.setLatestReplyUser(userMap.get(interactionReplyMap.get(question.getLatestAnswerId()).getUserId()).getName());
+            }
+
+            result.add(questionVO);
+        }
+        return PageDTO.of(p, result);
     }
 }
