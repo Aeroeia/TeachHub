3509b2a4b6f7b6001cc2438561df92833e86c06c
Aeroeia <1269938269@qq.com>
2025-07-31 01:17:47 +0800

    新增优惠券

--- teachub-promotion/src/main/java/com/teachub/promotion/controller/CouponController.java
+++ teachub-promotion/src/main/java/com/teachub/promotion/controller/CouponController.java
@@ -1,8 +1,20 @@
 package com.teachub.promotion.controller;
 
 
+import com.teachub.promotion.domain.dto.CouponFormDTO;
+import com.teachub.promotion.service.ICouponService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * <p>
@@ -13,7 +25,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2025-07-30
  */
 @RestController
-@RequestMapping("/coupon")
+@RequestMapping("/coupons")
+@Api(tags = "优惠券相关接口")
+@RequiredArgsConstructor
+@Slf4j
 public class CouponController {
-
+    private final ICouponService couponService;
+    @PostMapping
+    @ApiOperation("新增优惠券")
+    public void addCoupon(@RequestBody @Validated CouponFormDTO coupon) {
+        log.info("新增优惠券：{}", coupon);
+        couponService.addCoupon(coupon);
+    }
 }
--- teachub-promotion/src/main/java/com/teachub/promotion/service/impl/CouponServiceImpl.java
+++ teachub-promotion/src/main/java/com/teachub/promotion/service/impl/CouponServiceImpl.java
@@ -1,10 +1,22 @@
 package com.teachub.promotion.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.teachub.common.exceptions.BadRequestException;
+import com.teachub.common.utils.BeanUtils;
+import com.teachub.common.utils.CollUtils;
+import com.teachub.promotion.domain.dto.CouponFormDTO;
 import com.teachub.promotion.domain.po.Coupon;
+import com.teachub.promotion.domain.po.CouponScope;
 import com.teachub.promotion.mapper.CouponMapper;
+import com.teachub.promotion.service.ICouponScopeService;
 import com.teachub.promotion.service.ICouponService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -15,6 +27,35 @@ import org.springframework.stereotype.Service;
  * @since 2025-07-30
  */
 @Service
+@RequiredArgsConstructor
 public class CouponServiceImpl extends ServiceImpl<CouponMapper, Coupon> implements ICouponService {
+    private final ICouponScopeService couponScopeService;
 
+    @Transactional
+    @Override
+    public void addCoupon(CouponFormDTO coupon) {
+        //转po
+        Coupon po = BeanUtils.copyBean(coupon, Coupon.class);
+        this.save(po);
+        //判断是否有限定范围
+        if (coupon.getSpecific() == null || !coupon.getSpecific()) {
+            return;
+        }
+        //获取范围
+        List<Long> scopes = coupon.getScopes();
+        if (CollUtils.isEmpty(scopes)) {
+            throw new BadRequestException("限定范围为空");
+        }
+
+        List<CouponScope> list = scopes.stream().map(id -> {
+            CouponScope couponScope = new CouponScope();
+            couponScope.setCouponId(po.getId());
+            couponScope.setBizId(id);
+            //目前type固定为1
+            couponScope.setType(1);
+            return couponScope;
+        }).collect(Collectors.toList());
+        //新增优惠券范围
+        couponScopeService.saveBatch(list);
+    }
 }
